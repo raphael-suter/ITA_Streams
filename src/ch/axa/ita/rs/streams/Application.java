8ac153324dd7d4c5814def64de0129b8824a1b40
Raphael Suter <raphael.werner.suter@gmail.com>
2021-01-19 08:46:02 +0100

    Set up project

---
+++ src/ch/axa/ita/rs/streams/Application.java
@@ -0,0 +1,11 @@
+package ch.axa.ita.rs.streams;
+
+public class Application {
+    public Application() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        new Application();
+    }
+}
